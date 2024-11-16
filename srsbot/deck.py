1ccdf5eb64fd93cb4cc632a1db994bb4fc3ffc90
Taiixu <taiixu@outlook.com>
2024-11-16 21:34:53 +0300

    make decks work

--- srsbot/deck.py
+++ srsbot/deck.py
@@ -6,12 +6,18 @@ import random
 class SimpleDeck:
     def __init__(self, path):
         self.deck_path = path
+        self.deck_name = '.'.join(os.path.basename(path).split('.')[:-1])
         self.__csv_file = open(path, 'r', encoding='utf-8').read()
         self.selected_fields = ''
+        self.new_count = 15
         self.headers = self.__csv_file.split('\n')[0].split(',')
         self.fields = self.__csv_file.split('\n')[1:-1] if self.__csv_file.split('\n')[-1] == '' else self.__csv_file.split('\n')[1:]
         if not self.check_fields():
             self.add_deck_fields()
+        
+        self.___time = []
+        self.___status = []
+        self.add_time()
 
     def add_deck_fields(self):
         self.headers += ['___time', '___status', '___srs_time', '___card_time']
@@ -23,7 +29,6 @@ class SimpleDeck:
         for key, index in zip(self.headers[:-4], range(len(self.headers[:-4]))):
             column = []
             for item in self.fields:
-                # print(key)
                 column.append(item.split(',')[index])
             ret.update({key: column})
             column = []
@@ -34,12 +39,36 @@ class SimpleDeck:
     
     def add_selected_fields(self, question, answer):
         self.selected_fields = f'{question};{answer}'
+     
+    def get_current_day(self):
+        return int(time.time()) // (24 * 60 * 60)
+
+    def get_column(self, field_name):
+        index = self.headers.index(field_name)
+        ret = []
+        for c in self.fields:
+            ret.append(c.split(',')[index])
+        return ret
     
+    def add_time(self):
+        current_day = self.get_current_day()
+        self.___time = self.get_column('___time')
+        self.___status = self.get_column('___status')
+        for c in range(0, len(self.___time), self.new_count):
+            for t in range(c, c+15):
+                if t >= len(self.___time):
+                    break
+                self.___time[t] = str(current_day)
+                self.___status[t] = '1'
+            current_day += 1
+        
     def save(self):
         f = open(self.deck_path, 'w', encoding='utf-8')
         f.write(self.selected_fields + '\n')
         f.write(','.join(self.headers) + '\n')
-        f.write('\n'.join(self.fields))
+        # f.write('\n'.join(self.fields))
+        for i in range(len(self.fields)):
+            f.write(f"{','.join(self.fields[i].split(',')[:-4])},{self.___time[i]},{self.___status[i]},0,0\n")
         f.close()
 
 
@@ -55,6 +84,8 @@ class Deck:
     '''
     def __init__(self, path):
         self.deck_path = path
+        self.deck_name = '.'.join(os.path.basename(path).split('.')[:-1])
+        self.deck_name_without_id = self.deck_name.split(';', maxsplit=1)[1]
         self.__csv_file = open(path, 'r', encoding='utf-8').read()
         self.new_count = 15
         self.__qa = self.__csv_file.split('\n')[0].split(';')
@@ -77,12 +108,13 @@ class Deck:
         self.___card_time = self.get_column('___card_time')
 
         self.new, self.study_now, self.repeat = self.get_studying_cards()
-        self.count = self.get_studying_count()
-
         self.queue = self.list_sum(self.new, self.study_now, self.repeat)
+        self.count = self.get_studying_count()
         self.current_card = 0
 
     def move_to_center(self):
+        if len(self.queue) == 1:
+            return self.queue
         center_of_list = len(self.queue) // 2
         ret = self.queue[1:center_of_list] + [self.queue[0]] + self.queue[center_of_list:]
         return ret
@@ -114,12 +146,26 @@ class Deck:
             if len(ret) == count:
                 break
         return ret
-            
+    
+    def shuffle_answers(self, correct_answer, incorrect_answers):
+        l = [correct_answer] + incorrect_answers
+        ret = []
+        while len(l) != 0:
+            index = random.randint(0, len(l) - 1)
+            ret.append(l[index])
+            del l[index]
+        return ret
+
     def answer(self, ans):
         if self.answers[self.current_card] == ans:
+            if self.___status[self.current_card] == '1':
+                self.___status[self.current_card] = '2'
+                self.queue = self.move_to_center()
+                return True
             self.___status[self.current_card] = '3'
             self.___srs_time[self.current_card] = str(int(2.5 * int(self.___card_time[self.current_card]) + 1))
             self.___card_time[self.current_card] = '0'
+            self.___time[self.current_card] = str(self.get_current_day())
             del self.queue[0]
             return True
         else:
@@ -128,20 +174,17 @@ class Deck:
             self.queue = self.move_to_center()
             return False
         
-    def update(self):
-        self.new, self.study_now, self.repeat = self.get_studying_cards()
-        self.count = self.get_studying_count()
-
     def study(self):
         if len(self.queue) == 0:
             return None
+        # self.update()
         self.current_card = self.queue[0]
         question = self.questions[self.current_card]
         answer = self.answers[self.current_card]
         rand_answers = self.get_random_answers(3, answer)
         if rand_answers == None:
             raise Exception("get_random_answers count >= len(answers)")
-        return question, answer, rand_answers, self.count
+        return [question, answer, self.shuffle_answers(answer, rand_answers), self.get_studying_count()]
         
     def get_studying_cards(self):
         self.day_now = self.get_current_day()
@@ -152,7 +195,7 @@ class Deck:
 
         for i in range(len(self.questions)):
             if self.___time[i] != '' and self.___status[i] != '' and self.___srs_time[i] != '' and self.___card_time[i] != '':
-                if self.___status[i] == '1':
+                if self.___status[i] == '1' and int(self.___time[i]) <= self.day_now:
                     new.append(i)
                 elif self.___status[i] == '2':
                     studying.append(i)
@@ -176,7 +219,20 @@ class Deck:
         return int(time.time()) // (24 * 60 * 60)
 
     def get_studying_count(self):
-        return len(self.new), len(self.study_now), len(self.repeat)
+        ret = [0, 0, 0]
+        for i in self.queue:
+            try:
+                status = self.___status[i]
+            except:
+                break
+            if status == '1':
+                ret[0] += 1
+            elif status == '2':
+                ret[1] += 1
+            elif status == '3':
+                ret[2] += 1
+        return ret
+        # return len(self.new), len(self.study_now), len(self.repeat)
 
     def list_sum(self, l1, l2, l3):
         ret = []
--- srsbot/user.py
+++ srsbot/user.py
@@ -20,7 +20,8 @@ class UserBase:
 
     def update_db(self):
         for usr in self.__user_list:
-            self.base.update_user(str(usr.user_id), usr.lang_name, usr.decks, usr.stage)
+            deck_names = [c.deck_name for c in usr.decks]
+            self.base.update_user(str(usr.user_id), usr.lang.key, deck_names, usr.stage)
 
     def __upload_from_db(self):
         raw_db = self.base.get_db()
@@ -30,10 +31,12 @@ class UserBase:
             user.stage = u_info['stage']
             user.languages = self.langs
             for l in self.langs:
-                if l.name == u_info['lang']:
+                if l.key == u_info['lang']:
                     user.lang = l
                     user.lang_name = l.name
-            user.decks = u_info['decks'] # TODO: Make deck object
+            user.decks = []
+            for deck in u_info['decks']:
+                user.decks.append(Deck(f"{self.decks_path}/{deck}.csv"))
             self.__user_list.append(user)
             
     def is_user_exist(self, user_id):
@@ -60,6 +63,25 @@ class User:
         self.temp_path = temp_path
 
         self.add_deck_edit = None
+        self.fields = None
+        self.fields_list = None
+        self.question = ''
+        self.answer = ''
+
+        self.studying_deck = None
+        self.current_question = None
+    
+    def __sentence_short(self, text, length=40):
+        if len(text) < length:
+            return text
+        else:
+            return text[:40] + '...'
+
+    def __text_deck_fields(self):
+        ret = ''
+        for key, value in zip(self.fields.keys(), self.fields.values()):
+            ret += f"{key}: {self.__sentence_short(', '.join(value))}\n\n"
+        return ret[:-2]
     
     def __keyboard_setup_lang(self):
         markup = ReplyKeyboardMarkup(row_width=1)
@@ -68,6 +90,8 @@ class User:
     
     def __keyboard_main_menu(self):
         markup = ReplyKeyboardMarkup(row_width=1)
+        for c in self.decks:
+            markup.add(c.deck_name_without_id)
         add_deck = KeyboardButton(self.lang.print("buttonAddDeck"))
         settings = KeyboardButton(self.lang.print("buttonSettings"))
         markup.add(add_deck, settings)
@@ -79,11 +103,36 @@ class User:
         markup.add(back_button)
         return markup
 
+    def __keyboard_deck_fields(self):
+        markup = ReplyKeyboardMarkup(row_width=1)
+        for c in self.fields:
+            markup.add(KeyboardButton(c))
+        return markup
+    
+    def send_question(self, status=None):
+        prefix = ''
+        if status == False:
+            prefix = f"{self.lang.print("incorrectAnswer")}{self.current_question[1]}"
+        elif status == True:
+            prefix = f"{self.lang.print("correctAnswer")}"
+        self.current_question = self.studying_deck.study()
+        if self.current_question == None:
+            self.stage = "main"
+            self.send("endStudy", self.__keyboard_main_menu())
+            self.studying_deck.save()
+            return
+        markup = ReplyKeyboardMarkup()
+        for c in self.current_question[2]:
+            markup.add(KeyboardButton(c))
+        markup.add(self.lang.print("buttonBack"))
+        self.studying_deck.save()
+        self.__bot.send_message(self.user_id, f"{prefix}\n{self.current_question[0]}\n\n{'/'.join([str(c) for c in self.current_question[3]])}", reply_markup=markup)
+
     def setup(self):
         self.__bot.send_message(self.user_id, self.lang.print("languageSetup"), reply_markup=self.__keyboard_setup_lang())
 
-    def send(self, msg, keyboard):
-        self.__bot.send_message(self.user_id, self.lang.print(msg), reply_markup=keyboard)
+    def send(self, msg, keyboard, send_after=''):
+        self.__bot.send_message(self.user_id, self.lang.print(msg) + send_after, reply_markup=keyboard)
     
     def send_w_keyboard(self, msg):
         self.__bot.send_message(self.user_id, self.lang.print(msg))
@@ -118,7 +167,41 @@ class User:
                 self.send_w_keyboard("uploadError")
                 return
             
-            self.stage = "settings:decks_edit"
+            self.stage = "deck_add:fields_edit:q"
             self.add_deck_edit = SimpleDeck(f"{self.decks_path}/{self.user_id};{'.'.join(filename.split('.')[:-1])}.csv")
-            
-            
+            self.fields = self.add_deck_edit.get_fields()
+            self.fields_list = list(self.fields.keys())
+
+            self.send("deckUploadQuestionChoose", self.__keyboard_deck_fields(), send_after=f'\n{self.__text_deck_fields()}')
+        elif self.stage == 'deck_add:fields_edit:q':
+            if message.text not in self.fields_list:
+                self.send_w_keyboard("keyWrong")
+                return
+            self.question = message.text
+            self.stage = "deck_add:fields_edit:a"
+            self.send("deckUploadAnswerChoose", self.__keyboard_deck_fields(), send_after=f'\n{self.__text_deck_fields()}')
+        elif self.stage == "deck_add:fields_edit:a":
+            if message.text not in self.fields_list:
+                self.send_w_keyboard("keyWrong")
+                return
+            self.answer = message.text
+            self.add_deck_edit.add_selected_fields(self.question, self.answer)
+            self.add_deck_edit.save()
+            self.decks.append(Deck(self.add_deck_edit.deck_path))
+            self.stage = "main"
+            self.send("deckAddSuccess", self.__keyboard_main_menu())
+        elif self.stage == "main" and message.text in [c.deck_name_without_id for c in self.decks]:
+            for c in self.decks:
+                if c.deck_name_without_id == message.text:
+                    self.studying_deck = c
+            if len(self.studying_deck.queue) == 0:
+                self.send_w_keyboard("deckStudied")
+                return
+            self.stage = 'deck_study'
+            self.send_question()
+        elif self.stage == "deck_study" and message.text in self.current_question[2]:
+            isCorrect = self.studying_deck.answer(message.text)
+            self.send_question(status=isCorrect)
+        elif self.stage == "deck_study" and message.text == self.lang.print("buttonBack"):
+            self.stage = "main"
+            self.send("mainMenu", self.__keyboard_main_menu())
